3425762d935cfa920bd89b2b527ae0f9f589d3d1
muhammedmuneerk <muhammedmuneerk15@gmail.com>
2024-01-20 03:41:09 +0530

    everything perfect on  second time also

--- routes/admin.js
+++ routes/admin.js
@@ -37,9 +37,9 @@ router.get('/', function (req, res, next) {
 router.get('/add-product', function (req, res) {
   res.render('admin/add-product');
 });
-router.post('/add-product', upload.single('Image'), (req, res) => {
+router.post('/add-product',(req,res)=>{
   console.log(req.body);
-  console.log(req.file);
+  console.log(req.files.Image);
   res.send('File uploaded successfully!');
 })
 
